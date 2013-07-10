03675ef3c2a17159c13730f475b22762f320d231
Lydia Chen <lydialinglingchen@gmailcom>
2013-07-10 22:59:58 +0000

    updated web.js

--- web.js
+++ web.js
@@ -3,9 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+
  var fs= require('fs');
  var content  = fs.readFileSync('index.html','utf-8');
- response.send(buf.toString()); 
+ response.send(content.toString()); 
  
 });
 
@@ -13,4 +14,3 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-x
