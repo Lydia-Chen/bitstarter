8fe041ce15e7df04c3308a4ff405a60d67c47141
Lydia Chen <lydialinglingchen@gmailcom>
2013-07-10 00:05:46 +0000

    updated web.js

--- web.js
+++ web.js
@@ -3,8 +3,14 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
-});
+ var fs= require('fs');
+ var fileName = fs.readFileSync("index.html");
+ var buf = new Buffer.toString(fileName);
+ for (var i=0; i< fileName.length;i++) {
+ buf[i] = str.charCodeA(i);
+ 
+}
+ response.send(buf);xsx
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
