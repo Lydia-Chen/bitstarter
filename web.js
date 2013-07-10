956c0fb6aa3a56e23fbb7b2b64986912cc0cb034
Lydia Chen <lydialinglingchen@gmailcom>
2013-07-10 23:48:27 +0000

    updated web.js

--- web.js
+++ web.js
@@ -1,12 +1,12 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
+ 
+ var content  = fs.readFileSync('index.html');
 
-app.get('/', function(request, response) {
 
- var fs= require('fs');
- var content  = fs.readFileSync('index.html','utf-8');
- response.send(content.toString()); 
+xs app.get('/',function(request,response) {
+ response.send(content.toString("utf-8"xcxcxcxc)); 
  
 });
 
@@ -14,3 +14,4 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+xs
